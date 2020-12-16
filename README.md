01622e157af2a40a6300e29184207031220ec26a
impypots <72578093+impypots@users.noreply.github.com>
2020-12-15 21:40:37 -0700

    Fixed form problem, added more functionality
    It actually works now, yay.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
  Making a to do list for practice :)
 
  12/14 Breaking my head trying to get JS code to add an HTML paragraph element without immediately removing it. Effectively added nothing, but hey here's a commit.
+
+ 12/15 Made a lot more progress today. App now gives each paragraph element a random color, clicking an element crosses through it, and clicking it again causes it to fade away, either to the left or the right. Oh yeah, and it actually works.
--- index.js
+++ index.js
@@ -2,11 +2,37 @@
 let addButton = document.getElementById("add-button");
 let list = document.getElementById("list");
 let input = document.getElementById("input");
+let numberItems = 1
 
-addButton.addEventListener("click", () => {
+function addItem() {
 var para =  document.createElement("p");
-//let node = document.createTextNode();
-para.innerText = input.value;
-//para.appendChild(node);
+para.innerText = numberItems + ". " + input.value;
+numberItems += 1;
+input.value = "";
+para.style.color = '#' + Math.floor(Math.random()*16777215).toString(16);
+para.classList.add("list-item");
 list.appendChild(para);
-});
+para.addEventListener('click', ()=> {
+    if(para.style.textDecoration !== "line-through"){
+    para.style.textDecoration = "line-through"
+} else {
+    numberItems -= 1;
+    if(numberItems % 2 === 0){
+    para.style.animation = "fadeAway1 0.5s ease forwards 0.1s";
+    setTimeout(()=> {
+        para.innerText = '';
+    }, 500)
+} else {
+    para.style.animation = "fadeAway2 0.5s ease forwards 0.1s";
+    setTimeout(()=> {
+        para.innerText = '';
+    }, 500)
+}
+}
+})
+}
+
+let listItem = document.querySelector("p.list");
+addButton.addEventListener("click", addItem);
+
+
